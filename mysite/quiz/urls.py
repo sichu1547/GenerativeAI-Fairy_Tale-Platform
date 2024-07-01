c22872ef15337dcbbb10c66f5f8a4a67d363125e
1stone <sichu1547@gmail.com>
2024-07-01 17:38:17 +0900

    quiz_appname누락 수정

--- mysite/quiz/urls.py
+++ mysite/quiz/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import QuizView
 from . import views
+
+app_name = 'quiz'
+
 urlpatterns = [
     path('<int:id>/', QuizView.as_view(), name='quiz_view'),
     path('', views.index, name='index'),
