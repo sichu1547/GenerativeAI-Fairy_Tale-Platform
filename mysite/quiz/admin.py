c5ac4d521ff463addcc92e2962f043255de27518
1stone <sichu1547@gmail.com>
2024-07-01 17:21:30 +0900

    동화 카테고리별로 나눔

--- mysite/quiz/admin.py
+++ mysite/quiz/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import ReaderStory
 
 class ReaderStoryAdmin(admin.ModelAdmin):
-    list_display = ('category', 'title', 'body')
+    list_display = ('category', 'title')
 
 admin.site.register(ReaderStory, ReaderStoryAdmin)
--- mysite/quiz/urls.py
+++ mysite/quiz/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import QuizView
 
+app_name = 'quiz'
 urlpatterns = [
-    path('', QuizView.as_view(), name='quiz_view'),
+    path('<int:id>/', QuizView.as_view(), name='quiz_view'),
 ]
