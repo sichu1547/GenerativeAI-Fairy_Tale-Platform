442fd3503de59938b8e9ce50da47513576582298
ChangChang <como411s@naver.com>
2024-07-02 16:50:07 +0900

    some update for docker

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env.dev')
+)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^x%9=jpc=rdh*uu)04cv@a$2d)o%lli$bc#8bf3nv+wc$)d8t="
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 # DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -96,7 +104,6 @@ MIDDLEWARE = [
 # }
 ROOT_URLCONF = "mysite.urls"
 
-import os
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
