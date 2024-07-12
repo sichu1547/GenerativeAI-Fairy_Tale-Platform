f3be0ea50a4bf03168877609d1053faae796188d
choi <cse05099@naver.com>
2024-07-12 13:38:01 +0900

    출석체크

--- mysite/myaccount/context_processors.py
+++ mysite/myaccount/context_processors.py
@@ -1,12 +1,14 @@
 from .models import Profile
 
 def profile_name(request):
+    context = {'profile_name': None, 'attendance_dates': []}
     if request.user.is_authenticated:
         profile_id = request.session.get('selected_profile_id')
         if profile_id:
             try:
                 profile = Profile.objects.get(id=profile_id, user=request.user)
-                return {'profile_name': profile.name}
+                context['profile_name'] = profile.name
+                context['attendance_dates'] = profile.attendance_dates
             except Profile.DoesNotExist:
                 pass
-    return {'profile_name': None}
+    return context
