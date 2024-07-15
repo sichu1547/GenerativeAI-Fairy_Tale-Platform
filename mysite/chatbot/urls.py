40bc70d0f395f3a472e4d64f499b67e56a8beda2
wonseok <sichu1547@gmail.com>
2024-07-15 16:52:07 +0900

    커튼 오류 수정 및 별점 추가

--- mysite/chatbot/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/', views.chatbot_api, name='chatbot_api'),
-]
--- mysite/starporint/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StarporintConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "starporint"
