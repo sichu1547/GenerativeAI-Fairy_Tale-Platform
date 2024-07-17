11bcc5bd84238edee5a0d8391e02bccf406abdbf
1stone <sichu1547@gmail.com>
2024-07-17 16:53:07 +0900

    동화읽기 이미지생성 테스트

--- mysite/reader/utils.py
+++ mysite/reader/utils.py
@@ -5,26 +5,26 @@ from django.conf import settings
 from openai import OpenAI
 
 def generate_image(sentence):
-    # print('생성중')
-    # api_key = settings.OPENAI_API_KEY
-    # client = OpenAI(api_key = api_key)
+    print('생성중')
+    api_key = settings.OPENAI_API_KEY
+    client = OpenAI(api_key = api_key)
     
-    # try:
-    #     response = client.images.generate(
-    #         model="dall-e-3",
-    #         prompt=f"This: {sentence}. Based on this, draw a cute and lovely 2D picture using an illustration that never contains text, phrases, or speech bubbles. Never add text.",
-    #         #prompt=f"Here is the text of a fairy tale: {sentence}. Based on this text, create an illustration for the story. Draw in a hand-drawn style with soft colors, simplified shapes.",
-    #         size="1024x1024",
-    #         n=1,
-    #         quality="standard",
-    #         style="natural"
-    #     )
-    #     image_url = response.data[0].url
-    #     print('성공')
-    #     return image_url
+    try:
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=f"Here is the text of a fairy tale: {sentence}. Create a friendly and cute fairy tale scene image based on this sentence. There are no letters, symbols, or any kind of text in the image. Design this scene to look inviting and welcoming to children with a cute and joyful atmosphere. The image must come out as one scene in one image.",
+            #prompt=f"Here is the text of a fairy tale: {sentence}. Based on this text, create an illustration for the story. Draw in a hand-drawn style with soft colors, simplified shapes.",
+            size="1024x1024",
+            n=1,
+            quality="standard",
+            style="vivid"
+        )
+        image_url = response.data[0].url
+        print('성공')
+        return image_url
 
-    # except Exception as e:
-    #     print('실패')
+    except Exception as e:
+        print('실패', e)
     return ""
 
 def generate_tts(request, ssml_text):
