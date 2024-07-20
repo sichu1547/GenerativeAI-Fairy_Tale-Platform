86f99d8e2f9ed4261ea581aac156f33ad97167da
ChangChang <como411s@naver.com>
2024-07-20 09:46:22 +0900

    생성동화 관리자페이지 추가

--- mysite/generator/admin.py
+++ mysite/generator/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.urls import path
+from .models import *
 
-# Register your models here.
+
+class GenStoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    ordering = ['id']
+    search_fields = ['body']
+    #list_per_page = 3
+
+admin.site.register(GenStory, GenStoryAdmin)
