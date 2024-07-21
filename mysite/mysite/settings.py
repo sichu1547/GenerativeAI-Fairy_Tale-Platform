e6eb64e7dc0ecb4458c4558f5566c5c03b49e0f5
ChangChang <como411s@naver.com>
2024-07-21 13:31:07 +0900

    media 경로 설정

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -267,6 +267,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static',
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
